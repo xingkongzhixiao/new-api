3c36a17403601382c7b0e0efc7b167d1c53ff96b
xingkongzhixiao <1647568648@qq.com>
2026-06-14 16:55:21 +0800

    更换图标和网站名称

--- main.go
+++ main.go
@@ -49,7 +49,7 @@ func main() {
 		return
 	}
 
-	common.SysLog("New API " + common.Version + " started")
+	common.SysLog("AIGCCode " + common.Version + " started")
 	if os.Getenv("GIN_MODE") != "debug" {
 		gin.SetMode(gin.ReleaseMode)
 	}
